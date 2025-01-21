758ad271a9638d1bcb3495ca91e38b7d009735af
hayley-d <hayleydod@proton.me>
2025-01-21 13:08:14 +0200

    starting consumer

--- ingestion/KafkaConsumer.java
+++ ingestion/KafkaConsumer.java
@@ -1,2 +1,8 @@
+package com.cloudurable.kafka;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
 public class KafkaConsumer {
 }
