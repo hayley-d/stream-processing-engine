4dfa96fb01dcfe8565c27b3dbc4929d2f8e185f4
hayley-d <hayleydod@proton.me>
2025-01-21 13:18:39 +0200

    starting consumer

--- ingestion/KafkaConsumer.java
+++ ingestion/KafkaConsumer.java
@@ -3,6 +3,9 @@ import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import java.util.Collections;
+import java.util.Properties;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class KafkaConsumer {
 }
--- ingestion/Main.java
+++ ingestion/Main.java
@@ -1,5 +1,28 @@
+import io.github.cdimascio.dotenv.Dotenv;
+package com.cloudurable.kafka;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.util.Collections;
+import java.util.Properties;
+
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello world");
+        Dotenv dotenv = Dotenv.load();
+        String topic = dotenv.get("KAFKA_TOPIC");
+        String topic = dotenv.get("KAFKA_TOPIC");
+        String bootstrap = dotenv.get("KAFKA_BOOTSTRAP");
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers",dotenv.get("KAFKA_BROKER"));
+        props.put("group.id",dotenv.get("KAFKA_GROUP_ID"));
+        props.put("key.deserializer",LongDeserializer.class.getName());
+        props.put("value.deserializer",StringDeserializer.class.getName());
+
+        final Consumer<Long,String> consumer = new KafkaConsumer<>(props);
+
+
+
     }
 }
